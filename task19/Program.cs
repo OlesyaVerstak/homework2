4cf576211180ee3eca3723bcb865646c34d3af81
Olesya <o.verstak99@gmail.com>
2022-12-07 18:47:18 +0700

    homework done

---
+++ task19/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+System.Console.WriteLine("Введите пятизначное число: ");
+string n = Convert.ToString(Console.ReadLine());
+if (n[0]==n[4] && n[1]==n[3])
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+System.Console.WriteLine("Число не является палиндромом");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int[] point = new int[6];
+System.Console.WriteLine("Введите координату 1 точки по оси Х: ");
+point[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату 1 точки по оси Y: ");
+point[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату 1 точки по оси Z: ");
+point[2] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату 2 точки по оси Х: ");
+point[3] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату 2 точки по оси Y: ");
+point[4] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату 2 точки по оси Z: ");
+point[5] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Math.Sqrt(Math.Pow(point[3] - point[0], 2) + Math.Pow(point[4] - point[1], 2) + Math.Pow(point[5] - point[2], 2)));
